57d9cacf0070dce953af9a7e44c37f51222ba958
Checo03 <Valdivia_sergio@outlook.com>
2024-05-06 16:05:11 -0600

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterModule,RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { HttpClient } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,MenuComponent, RouterModule],
+  imports: [RouterOutlet,HomeComponent,MenuComponent, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
